de57a830de55164e36decbfa47ef28d392dd1e44
dmoyaco <dmoyaco@85612454-b242-0410-874a-7da6d9b49ce3>
2008-02-28 00:47:15 +0000

    Parte de David. Miradlo para ver q os parece.

--- trunk/inmorate/gui/VistaMatrimonio.java
+++ trunk/inmorate/gui/VistaMatrimonio.java
@@ -9,33 +9,38 @@ import javax.swing.border.*;
 
 import inmorate.controlador.Controlador;
 
-public class VistaMatrimonio extends JDialog {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5133213368903256893L;
+public class VistaMatrimonio extends JFrame {
 	
+	private static final long serialVersionUID = -5133213368903256893L;	
 	private Controlador controlador;
+	private JButton buttonAceptar;
+	private JButton buttonCancelar;
+	private JLabel labelTitulo;
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
+	private JComboBox comboBox1;
+	private JComboBox comboBox2;
+	private JComboBox comboBox3;
 
 	public VistaMatrimonio(Controlador control) {
 		this.controlador = control;
 		initComponents();
-		setSize(400,330);
+		setSize(510,420);
+		setLocation(300,150);
 		setVisible(false);
-		setModal(true);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		buttonCancelar.addActionListener(
 				new ActionListener() {
 					public void actionPerformed(ActionEvent evento) {
-						controlador.cerrarPrograma();
+						controlador.volverAtras();
 					}
 				} // ActionListener			
 		); // buttonCancelar.addActionListener
 	}
 
 	private void initComponents() {
-		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 		buttonAceptar = new JButton();
 		buttonCancelar = new JButton();
 		labelTitulo = new JLabel();
@@ -54,43 +59,43 @@ public class VistaMatrimonio extends JDialog {
 		//---- buttonAceptar ----
 		buttonAceptar.setText("Aceptar");
 		buttonAceptar.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.darkGray, null));
-		buttonAceptar.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		buttonAceptar.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		contentPane.add(buttonAceptar);
-		buttonAceptar.setBounds(20, 250, 165, 30);
+		buttonAceptar.setBounds(30, 320, 215, 40);
 
 		//---- buttonCancelar ----
 		buttonCancelar.setText("Cancelar");
 		buttonCancelar.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.darkGray, null));
-		buttonCancelar.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		buttonCancelar.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		contentPane.add(buttonCancelar);
-		buttonCancelar.setBounds(205, 250, 165, 30);
+		buttonCancelar.setBounds(255, 320, 215, 40);
 
 		//---- labelTitulo ----
-		labelTitulo.setText("Valores de mayor a menor importancia:");
-		labelTitulo.setFont(new Font("Arial Unicode MS", Font.BOLD, 20));
+		labelTitulo.setText("Valore de mayor a menor importancia:");
+		labelTitulo.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
 		contentPane.add(labelTitulo);
-		labelTitulo.setBounds(new Rectangle(new Point(10, 15), labelTitulo.getPreferredSize()));
+		labelTitulo.setBounds(new Rectangle(new Point(60, 30), labelTitulo.getPreferredSize()));
 
 		//---- label1 ----
 		label1.setText("Estado del Portal");
-		label1.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		label1.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		contentPane.add(label1);
-		label1.setBounds(new Rectangle(new Point(40, 65), label1.getPreferredSize()));
+		label1.setBounds(new Rectangle(new Point(50, 100), label1.getPreferredSize()));
 
 		//---- label2 ----
 		label2.setText("Num. Ba\u00f1os / Aseos");
-		label2.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		label2.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		contentPane.add(label2);
-		label2.setBounds(new Rectangle(new Point(40, 130), label2.getPreferredSize()));
+		label2.setBounds(new Rectangle(new Point(50, 175), label2.getPreferredSize()));
 
 		//---- label3 ----
 		label3.setText("Altura (Planta)");
-		label3.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		label3.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		contentPane.add(label3);
-		label3.setBounds(new Rectangle(new Point(40, 195), label3.getPreferredSize()));
+		label3.setBounds(new Rectangle(new Point(50, 250), label3.getPreferredSize()));
 
 		//---- comboBox1 ----
-		comboBox1.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		comboBox1.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		comboBox1.setMaximumRowCount(3);
 		comboBox1.setModel(new DefaultComboBoxModel(new String[] {
 			"Muy Importante",
@@ -99,10 +104,10 @@ public class VistaMatrimonio extends JDialog {
 		}));
 		comboBox1.setMaximumSize(new Dimension(130, 25));
 		contentPane.add(comboBox1);
-		comboBox1.setBounds(205, 60, 165, comboBox1.getPreferredSize().height);
+		comboBox1.setBounds(270, 95, 185, comboBox1.getPreferredSize().height);
 
 		//---- comboBox2 ----
-		comboBox2.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		comboBox2.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		comboBox2.setMaximumRowCount(3);
 		comboBox2.setModel(new DefaultComboBoxModel(new String[] {
 			"Muy Importante",
@@ -111,10 +116,10 @@ public class VistaMatrimonio extends JDialog {
 		}));
 		comboBox2.setMaximumSize(new Dimension(130, 25));
 		contentPane.add(comboBox2);
-		comboBox2.setBounds(205, 125, 165, comboBox2.getPreferredSize().height);
+		comboBox2.setBounds(270, 170, 185, comboBox2.getPreferredSize().height);
 
 		//---- comboBox3 ----
-		comboBox3.setFont(new Font("Arial Unicode MS", Font.PLAIN, 14));
+		comboBox3.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
 		comboBox3.setMaximumRowCount(3);
 		comboBox3.setModel(new DefaultComboBoxModel(new String[] {
 			"Muy Importante",
@@ -123,7 +128,7 @@ public class VistaMatrimonio extends JDialog {
 		}));
 		comboBox3.setMaximumSize(new Dimension(130, 25));
 		contentPane.add(comboBox3);
-		comboBox3.setBounds(205, 190, 165, comboBox3.getPreferredSize().height);
+		comboBox3.setBounds(270, 245, 185, comboBox3.getPreferredSize().height);
 
 		{ // compute preferred size
 			Dimension preferredSize = new Dimension();
@@ -137,18 +142,5 @@ public class VistaMatrimonio extends JDialog {
 			preferredSize.height += insets.bottom;
 			contentPane.setPreferredSize( preferredSize );
 		}
-		// JFormDesigner - End of component initialization  //GEN-END:initComponents
 	}
-
-	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-	private JButton buttonAceptar;
-	private JButton buttonCancelar;
-	private JLabel labelTitulo;
-	private JLabel label1;
-	private JLabel label2;
-	private JLabel label3;
-	private JComboBox comboBox1;
-	private JComboBox comboBox2;
-	private JComboBox comboBox3;
-	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
