1e99f4fd2dea84d6bd33bc6e13ef2a3371b9bb84
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@85612454-b242-0410-874a-7da6d9b49ce3>
2008-05-18 17:28:48 +0000

    - Modificaciones necesarias para exportar en formato CSV. - Eliminados ficheros innecesarios

--- trunk/inmorate/model/InmuebleValorado.java
+++ trunk/inmorate/model/InmuebleValorado.java
@@ -1,12 +1,16 @@
 package inmorate.model;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+import inmorate.controlador.elementos.Elementos;
 import inmorate.controlador.valoracion.Valoracion;
+import inmorate.core.MotorInferencia;
+import static inmorate.controlador.constants.CONSTANTS.SEPARATOR;
 
 public class InmuebleValorado {
 	private static Logger logger = Logger.getLogger(InmuebleValorado.class);
@@ -51,22 +55,24 @@ public class InmuebleValorado {
 	public String toStringOnlyValues() {
 		String datosDebug = "";
 		for (int i = 0; i < valoraciones.length; i++)
-			datosDebug += valoraciones[i].getValor() + "\t";
+			datosDebug += valoraciones[i].getValor() + SEPARATOR;
 		
 		return inmueble.getDMXX() + 
-				"\t" + valoracionGeneral.getValor() +
-				"\t" + valoracionUsuario.getValor() +
-				"\t" + valoracionExperto.getValor() +
-				"\t" + datosDebug; 
+				SEPARATOR + valoracionGeneral.getValor() +
+				SEPARATOR + valoracionUsuario.getValor() +
+				SEPARATOR + valoracionExperto.getValor() +
+				SEPARATOR + datosDebug; 
 	}
 	public String toStringOnlyValuesFuzzy() {
 		String datosDebug = "";
 		for (int i = 0; i < valoraciones.length; i++)
-			datosDebug += valoraciones[i].fuzzy().toString() + "\t";
+			datosDebug += valoraciones[i].fuzzy().toString() + SEPARATOR;
 		
 		return inmueble.getDMXX() + 
-				"\t" + valoracionGeneral.getValor() +
-				"\t" + datosDebug; 
+				SEPARATOR + valoracionGeneral.fuzzy() +
+				SEPARATOR + valoracionUsuario.fuzzy() +
+				SEPARATOR + valoracionExperto.fuzzy() +
+				SEPARATOR + datosDebug; 
 	}
 
 	public Inmueble getInmueble() {
@@ -118,30 +124,30 @@ public class InmuebleValorado {
 		BufferedWriter bw = null;
 		try {
 			bw = new BufferedWriter(new FileWriter(sFichero));
-			bw.write("CLAVE\t");
-			bw.write("Valoracion General\t");
-			bw.write("Valoracion Usuario\t");
-			bw.write("Valoracion Experto\t");
-			bw.write("TipoInmueble\t");
-			bw.write("Luminosidad\t");
-			bw.write("Orientacion\t");
-			bw.write("Representatividad\t");
-			bw.write("EstadoDelPortal\t");
-			bw.write("Fachada\t");
-			bw.write("Vistas\t");
-			bw.write("NumeroHabitaciones\t");
-			bw.write("numeroBanos\t");
-			bw.write("Antiguedad\t");
-			bw.write("Altura\t");
-			bw.write("Ascensor\t");
-			bw.write("Garaje\t");
-			bw.write("EstadoGeneral\t");
-			bw.write("ZonasComunes\t");
-			bw.write("ZonaSubzona\t");
-			bw.write("MetrosConstruidos\t");
-			bw.write("MetrosHabitables\t");
-			bw.write("PrecioTasacion\t");
-			bw.write("PrecioSalida\t");
+			bw.write("CLAVE" + SEPARATOR);
+			bw.write("Valoracion General" + SEPARATOR);
+			bw.write("Valoracion Usuario" + SEPARATOR);
+			bw.write("Valoracion Experto" + SEPARATOR);
+			bw.write("TipoInmueble" + SEPARATOR);
+			bw.write("Luminosidad" + SEPARATOR);
+			bw.write("Orientacion" + SEPARATOR);
+			bw.write("Representatividad" + SEPARATOR);
+			bw.write("EstadoDelPortal" + SEPARATOR);
+			bw.write("Fachada" + SEPARATOR);
+			bw.write("Vistas" + SEPARATOR);
+			bw.write("NumeroHabitaciones" + SEPARATOR);
+			bw.write("numeroBanos" + SEPARATOR);
+			bw.write("Antiguedad" + SEPARATOR);
+			bw.write("Altura" + SEPARATOR);
+			bw.write("Ascensor" + SEPARATOR);
+			bw.write("Garaje" + SEPARATOR);
+			bw.write("EstadoGeneral" + SEPARATOR);
+			bw.write("ZonasComunes" + SEPARATOR);
+			bw.write("ZonaSubzona" + SEPARATOR);
+			bw.write("MetrosConstruidos" + SEPARATOR);
+			bw.write("MetrosHabitables" + SEPARATOR);
+			bw.write("PrecioTasacion" + SEPARATOR);
+			bw.write("PrecioSalida" + SEPARATOR);
 			bw.write("PrecioVenta\n");
 			
 			for (int i = 0; i < inmueblesValorados.length; i++){
